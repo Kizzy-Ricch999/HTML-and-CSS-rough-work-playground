5f172b30b0aa6a80cc4adb1c1b696b8a14bf70ec
Kizzy-Ricch999 <108525546+Kizzy-Ricch999@users.noreply.github.com>
2022-08-12 08:17:04 +0100

    Deleted files

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form')
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    console.log('starting submiting form')
-    const inputs = document.querySelectorAll('input')
-
-    console.log(inputs)
-})
